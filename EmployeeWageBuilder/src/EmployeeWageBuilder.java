ee8c19e32d1505a0db47679bc4791e9edbbf2f5b
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 12:25:12 +0530

    Add:Swtich Case added Change:Converted int to final int cause of switch case

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -4,17 +4,23 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to UC1-Employee Check");
-		 int IS_FULL_TIME = 1;
-		 int IS_PART_TIME = 2;
+		 final int IS_FULL_TIME = 1;
+		 final int IS_PART_TIME = 2;
 		 float employeeHours = 0;
 		 float employeeRatePerHr=20 , salary = 0;
 	        double empCheck = Math.floor(Math.random()*10) % 3;
-	        if(empCheck == IS_FULL_TIME) {
+	        switch((int)empCheck) {
+	        case IS_FULL_TIME:
 	        	employeeHours = 8;
-	        }
-	        else if(empCheck == IS_PART_TIME) {
-	        	
+	        	break;
+	        
+	        case IS_PART_TIME:
 	        	employeeHours = 4;
+	        	break;
+	        	
+	        default:
+	        	employeeHours = 0;
+	        	break;
 	        }
 	        salary=employeeHours*employeeRatePerHr;
 	        System.out.println("Salary of Employee is "+salary);
