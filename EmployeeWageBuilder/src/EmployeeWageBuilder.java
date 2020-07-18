1994060b2a5669d47aea4d7bcfc0b08873dc2a1a
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 12:12:55 +0530

    Change:Set employee hours accodring to employee status change:Calculate salary of employee outside if

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -5,12 +5,18 @@ public class EmployeeWageBuilder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to UC1-Employee Check");
 		 int IS_FULL_TIME = 1;
-		 float employeeHours = 8;
+		 int IS_PART_TIME = 2;
+		 float employeeHours = 0;
 		 float employeeRatePerHr=20 , salary = 0;
-	        double empCheck = Math.floor(Math.random()*10) % 2;
+	        double empCheck = Math.floor(Math.random()*10) % 3;
 	        if(empCheck == IS_FULL_TIME) {
-	        	salary=employeeHours*employeeRatePerHr;
+	        	employeeHours = 8;
 	        }
+	        else if(empCheck == IS_PART_TIME) {
+	        	
+	        	employeeHours = 4;
+	        }
+	        salary=employeeHours*employeeRatePerHr;
 	        System.out.println("Salary of Employee is "+salary);
 
 	}
