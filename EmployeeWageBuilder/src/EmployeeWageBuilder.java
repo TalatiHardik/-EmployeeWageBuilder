4db7f891c431f73c9a579694e7b206d0a9f86a72
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 11:58:49 +0530

    Add:Random Function UC1 completed

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -3,7 +3,14 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to master branch");
+		System.out.println("Welcome to UC1-Employee Check");
+		 int IS_FULL_TIME = 1;
+	        double empCheck = Math.floor(Math.random()*10) % 2;
+	        if(empCheck == IS_FULL_TIME)
+	                System.out.println("Employee is Present");
+	        else
+	                System.out.println("Employee is Absent");
+
 
 	}
 
