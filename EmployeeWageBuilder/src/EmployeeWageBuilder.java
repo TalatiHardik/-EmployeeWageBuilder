7441ad2f5e13bd7054b8f636c7756ae8a77b3642
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 12:53:29 +0530

    Changed:Print statement changed in UC3

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -3,7 +3,7 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to UC1-Employee Check");
+		System.out.println("Welcome to UC3-Add part time employee");
 		 int IS_FULL_TIME = 1;
 		 int IS_PART_TIME = 2;
 		 float employeeHours = 0;
