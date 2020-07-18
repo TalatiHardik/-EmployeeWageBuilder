31cec34f49d7fc9bd52d385a677210e9d3dbd231
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 12:52:43 +0530

    Changed:Print statement changed in UC2

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -3,7 +3,7 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to UC1-Employee Check");
+		System.out.println("Welcome to UC2-Caculate daily wage");
 		 int IS_FULL_TIME = 1;
 		 float employeeHours = 8;
 		 float employeeRatePerHr=20 , salary = 0;
