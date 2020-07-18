981400561f74328b92c4a5aa35e03406ce0fbc87
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 12:50:22 +0530

    UC5 completed Add:For loop added for working days Add:Variable total salary added

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -3,12 +3,16 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to UC1-Employee Check");
+		System.out.println("Welcome to UC5-Add working days per month");
 		 final int IS_FULL_TIME = 1;
 		 final int IS_PART_TIME = 2;
-		 float employeeHours = 0;
+		 final int NUM_WORKING_DAYS = 20;
+		 float employeeHours = 0 , totalSalary=0;
 		 float employeeRatePerHr=20 , salary = 0;
-	        double empCheck = Math.floor(Math.random()*10) % 3;
+	     
+	     for(int day=1;day<NUM_WORKING_DAYS;day++) {
+	    	 
+	    	double empCheck = Math.floor(Math.random()*10) % 3;
 	        switch((int)empCheck) {
 	        case IS_FULL_TIME:
 	        	employeeHours = 8;
@@ -23,7 +27,11 @@ public class EmployeeWageBuilder {
 	        	break;
 	        }
 	        salary=employeeHours*employeeRatePerHr;
-	        System.out.println("Salary of Employee is "+salary);
+	        totalSalary=totalSalary+salary;
+	        
+	     }
+	        
+	        System.out.println("Salary of Employee is "+totalSalary);
 
 	}
 
