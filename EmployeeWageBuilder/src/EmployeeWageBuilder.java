bf56cde2642ae915e4d18f93bd645c9aec68ec12
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 13:05:52 +0530

    UC6 compelted Add:New variables added Chnaged:For loop changed to while loop

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -3,16 +3,20 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to UC5-Add working days per month");
+		System.out.println("Welcome to UC6-Add_condition_of_working_hours");
 		 final int IS_FULL_TIME = 1;
 		 final int IS_PART_TIME = 2;
 		 final int NUM_WORKING_DAYS = 20;
-		 float employeeHours = 0 , totalSalary=0;
-		 float employeeRatePerHr=20 , salary = 0;
+		 final int Max_Hrs_IN_Month = 100;
+		 
+		 float employeeHours = 0 , employeeRatePerHr=20;
+		 float totalSalary=0;
+		 float totalWorkingDays=0 , totalEmpHr=0 ;
 	     
-	     for(int day=1;day<NUM_WORKING_DAYS;day++) {
-	    	 
+	     while(totalEmpHr < Max_Hrs_IN_Month &&  totalWorkingDays < NUM_WORKING_DAYS) {
+	    	
 	    	double empCheck = Math.floor(Math.random()*10) % 3;
+	    	totalWorkingDays++;
 	        switch((int)empCheck) {
 	        case IS_FULL_TIME:
 	        	employeeHours = 8;
@@ -26,12 +30,11 @@ public class EmployeeWageBuilder {
 	        	employeeHours = 0;
 	        	break;
 	        }
-	        salary=employeeHours*employeeRatePerHr;
-	        totalSalary=totalSalary+salary;
-	        
+	        totalEmpHr=totalEmpHr+employeeHours;
+	        System.out.println("asd "+employeeHours);
 	     }
-	        
-	        System.out.println("Salary of Employee is "+totalSalary);
+	     totalSalary=totalEmpHr*employeeRatePerHr;
+	     System.out.println("Salary of Employee is "+totalSalary);
 
 	}
 
