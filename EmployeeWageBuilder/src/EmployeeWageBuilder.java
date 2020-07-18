f8e3992de7d28e89227869ba50baf7755c9f1ac6
TalatiHardik <hardikhtalati@gmail.com>
2020-07-18 12:06:55 +0530

    Add:Salary and realated variables Add:Calculate salary of employee

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -5,12 +5,13 @@ public class EmployeeWageBuilder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to UC1-Employee Check");
 		 int IS_FULL_TIME = 1;
+		 float employeeHours = 8;
+		 float employeeRatePerHr=20 , salary = 0;
 	        double empCheck = Math.floor(Math.random()*10) % 2;
-	        if(empCheck == IS_FULL_TIME)
-	                System.out.println("Employee is Present");
-	        else
-	                System.out.println("Employee is Absent");
-
+	        if(empCheck == IS_FULL_TIME) {
+	        	salary=employeeHours*employeeRatePerHr;
+	        }
+	        System.out.println("Salary of Employee is "+salary);
 
 	}
 
