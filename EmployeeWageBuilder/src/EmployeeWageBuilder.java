7112dd9afeae47f9d4ad1c92b00ec2ac03176880
TalatiHardik <hardikhtalati@gmail.com>
2020-07-20 17:52:53 +0530

    Changed:Instance methods added and objects were created

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -1,16 +1,27 @@
 
 public class EmployeeWageBuilder {
-	final static int IS_FULL_TIME = 1;
-	 final static int IS_PART_TIME = 2;
-	 final static int NUM_WORKING_DAYS = 20;
-	 final static int Max_Hrs_IN_Month = 100;
+	final int IS_FULL_TIME = 1;
+	 final int IS_PART_TIME = 2;
 	 
-	 static float employeeHours = 0;
-	
+	 float employeeHours = 0 , employeeRatePerHr=20;
+	 float totalSalary=0;
+	 float totalWorkingDays=0 , totalEmpHr=0 ;
+	 int numOfDays=0 , maxHrs=0;
+	 String companyName=""; 
 	 
+	 EmployeeWageBuilder(String name , float employeeRatePerHr , int numOfDays , int maxHrs){
+		 companyName=name;
+		 this.employeeRatePerHr=employeeRatePerHr;
+		 this.numOfDays=numOfDays;
+		 this.maxHrs=maxHrs;
+	 }
 	 
+	 EmployeeWageBuilder(){
+		 companyName="";
+		 
+	 }
 	 
-	 public static float getWorkingHours(double empCheck){
+	 public float getWorkingHours(double empCheck){
 		 switch((int)empCheck) {
 	        case IS_FULL_TIME:
 	        	employeeHours = 8;
@@ -28,14 +39,13 @@ public class EmployeeWageBuilder {
 		 return employeeHours;
 	 }
 	 
-	 public static void getSalary() {
-		 float employeeRatePerHr=20;
-		 float totalSalary=0;
-		 float totalWorkingDays=0 , totalEmpHr=0 ;
-		
+	 public void getSalary() {
 		 
+		 if(companyName!="") {
+			 System.out.println("Employee is from "+companyName);
+		 }
 		 System.out.println("Daily wages are: ");
-		 while(totalEmpHr < Max_Hrs_IN_Month &&  totalWorkingDays < NUM_WORKING_DAYS) {
+		 while(totalEmpHr < maxHrs &&  totalWorkingDays < numOfDays) {
 		    	
 		    	double empCheck = Math.floor(Math.random()*10) % 3;
 		    	totalWorkingDays++;
@@ -44,9 +54,16 @@ public class EmployeeWageBuilder {
 		        System.out.println("Day "+totalWorkingDays+" \t wage: "+(employeeHours*employeeRatePerHr));
 		     }
 		 
-		     totalSalary=totalEmpHr*employeeRatePerHr; 
-		     System.out.println("Total Salary of Employee is "+totalSalary);
-		   
+		     totalSalary=totalEmpHr*employeeRatePerHr;
+		     
+		     if(companyName!="") {
+		    	 
+		    	 System.out.println("Total Salary of "+companyName+" employee is "+totalSalary);
+			 }
+		     else{
+		    	 
+		    	 System.out.println("Total Salary of Employee is "+totalSalary);
+		     }
 	 }
 	 
     
@@ -54,7 +71,10 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to UC7-Use_OOP_Concepts");	 
-		 getSalary();
+		 EmployeeWageBuilder emp1 = new EmployeeWageBuilder("TCS",20,10,10);
+		 emp1.getSalary();
+		 EmployeeWageBuilder emp2 = new EmployeeWageBuilder("Wipro",22,9,12);
+		 emp2.getSalary();
 		 
 	     
 
