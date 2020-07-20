ff991a6e8ecd21a2f803c381200a13728a76246b
TalatiHardik <hardikhtalati@gmail.com>
2020-07-20 17:42:31 +0530

    Changed:Instance methods were changed to class method

--- EmployeeWageBuilder/src/EmployeeWageBuilder.java
+++ EmployeeWageBuilder/src/EmployeeWageBuilder.java
@@ -1,26 +1,16 @@
 
 public class EmployeeWageBuilder {
-	final int IS_FULL_TIME = 1;
-	 final int IS_PART_TIME = 2;
-	 final int NUM_WORKING_DAYS = 20;
-	 final int Max_Hrs_IN_Month = 100;
+	final static int IS_FULL_TIME = 1;
+	 final static int IS_PART_TIME = 2;
+	 final static int NUM_WORKING_DAYS = 20;
+	 final static int Max_Hrs_IN_Month = 100;
 	 
-	 float employeeHours = 0 , employeeRatePerHr=20;
-	 float totalSalary=0;
-	 float totalWorkingDays=0 , totalEmpHr=0 ;
-	 String employeeName=""; 
+	 static float employeeHours = 0;
+	
 	 
-	 EmployeeWageBuilder(String name){
-		 employeeName=name;
-		 
-	 }
 	 
-	 EmployeeWageBuilder(){
-		 employeeName="";
-		 
-	 }
 	 
-	 public float getWorkingHours(double empCheck){
+	 public static float getWorkingHours(double empCheck){
 		 switch((int)empCheck) {
 	        case IS_FULL_TIME:
 	        	employeeHours = 8;
@@ -38,11 +28,12 @@ public class EmployeeWageBuilder {
 		 return employeeHours;
 	 }
 	 
-	 public void getSalary() {
+	 public static void getSalary() {
+		 float employeeRatePerHr=20;
+		 float totalSalary=0;
+		 float totalWorkingDays=0 , totalEmpHr=0 ;
+		
 		 
-		 if(employeeName!="") {
-			 System.out.println("Employee name is "+employeeName);
-		 }
 		 System.out.println("Daily wages are: ");
 		 while(totalEmpHr < Max_Hrs_IN_Month &&  totalWorkingDays < NUM_WORKING_DAYS) {
 		    	
@@ -53,16 +44,9 @@ public class EmployeeWageBuilder {
 		        System.out.println("Day "+totalWorkingDays+" \t wage: "+(employeeHours*employeeRatePerHr));
 		     }
 		 
-		     totalSalary=totalEmpHr*employeeRatePerHr;
-		     
-		     if(employeeName!="") {
-		    	 
-		    	 System.out.println("Total Salary of "+employeeName+" is "+totalSalary);
-			 }
-		     else{
-		    	 
-		    	 System.out.println("Total Salary of Employee is "+totalSalary);
-		     }
+		     totalSalary=totalEmpHr*employeeRatePerHr; 
+		     System.out.println("Total Salary of Employee is "+totalSalary);
+		   
 	 }
 	 
     
@@ -70,8 +54,7 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to UC7-Use_OOP_Concepts");	 
-		 EmployeeWageBuilder emp1 = new EmployeeWageBuilder("Mukesh");
-		 emp1.getSalary();
+		 getSalary();
 		 
 	     
 
